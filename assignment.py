f656c0efae7c16374830810679be2401525d4ffd
tapokera <tapotandane@gmail.com>
2025-03-25 16:17:00 +0200

    initial commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+def find_pair_with_sum(arr, target):
+    seen = set()
+    for num in arr:
+        if target - num in seen:
+            print(f"Pair found: ({num}, {target - num})")
+            return
+        seen.add(num)
+    print("No pair found")
+
+arr = [1, 3, 5, 2, 4]
+target_sum = 9
+find_pair_with_sum(arr, target_sum)
